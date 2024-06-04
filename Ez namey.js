790670581075e53e146b79aa183a859ce262df1b
sathic <sathichowdhury2017@gmail.com>
2024-06-04 15:13:17 +0200

    Implemented task 1

---
+++ Ez namey.js
@@ -0,0 +1,22 @@
+let firstWords = ["Innovative",
+"Creative",
+"Dynamic",
+"Visionary",
+"NextGen",
+"Bright",
+"Future",
+"Tech",
+"Smart",
+"Global"];
+let secondWords = ["Solutions",
+"Enterprises",
+"Technologies",
+"Innovations",
+"Concepts",
+"Ventures",
+"Systems",
+"Networks",
+"Dynamics",
+"Strategies"];
+let startupName = firstWords[4] + " " + secondWords[9]
+console. log(`The startup: "${startupName} " contains ${startupName.length} characters`.);
