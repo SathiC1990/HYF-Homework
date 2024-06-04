05879b259d678103af4eb8cf181465984267d440
sathic <sathichowdhury2017@gmail.com>
2024-06-04 13:47:54 +0200

    Implemented task2

---
+++ dog-age.js
@@ -0,0 +1,5 @@
+let dogYearOfBirth = 2018;
+let dogYearFuture =  2025;
+let dogYear = dogYearFuture - dogYearOfBirth;
+ shouldShowResultInDogYears = "true";
+console. log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture);
