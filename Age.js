eea0b07ae4cc493b4e23781aaf11e7f071800324
sathic <sathichowdhury2017@gmail.com>
2024-06-05 19:11:44 +0200

    Implemented task 1

--- Age.js
+++ Age.js
@@ -1,5 +1,5 @@
 console.log("Homework-1");
-let yearOfBirth = 1990;
-let yearFuture = 2045;
-let age =  yearFuture-yearOfBirth;
-console.log("You will be " + age + " years old in " + yearFuture);
+const yearOfBirth = 1990;
+const yearFuture = 2045;
+const age =  yearFuture-yearOfBirth;
+console.log(`You will be ${age} years old in ${yearFuture}.`);
