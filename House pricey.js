d93353ab8d857de27a4abe93edadc499c5e375cb
sathic <sathichowdhury2017@gmail.com>
2024-06-06 14:37:06 +0200

    implemented task 3

--- House pricey.js
+++ House pricey.js
@@ -1,16 +1,25 @@
-let petersHouselengthInMeter = 8 ;
-let petersHousedepthInMeter = 10;
-let petesrHousewidthInMeyer = 10;
-let volumeOfPetersHouseInMeter = petersHouselengthInMeter*petersHousedepthInMeter*petesrHousewidthInMeyer;
- let petersHouseGardenSizeInM2 = 100;
- let house1cost = 2500000;
- let houseprice = volumeOfPetersHouseInMeter * 2.5 * 1000 + petersHouseGardenSizeInM2 * 300;
- console.log("Peter paying $" +  (house1cost-houseprice) + " lower price");
- let juliasHouselengthInMeter = 8;
-let juliaHousedepthInMeter = 11;
-let juliasHousewidthInMeter = 5;
-let volumeOfjuliasHouseInMeter = juliasHouselengthInMeter*juliaHousedepthInMeter*juliasHousewidthInMeter;
- let juliasHouseGardenSizeInM2 = 70;
- let houseprice2 = volumeOfjuliasHouseInMeter * 2.5 * 1000 + juliasHouseGardenSizeInM2 * 300;
- let house2cost = 1000000;
-console.log("Julia paying $" +  (houseprice2-house2cost) + " higher price");
+const petersHouselengthInMeter = 8 ;
+const petersHouseDepthInMeter = 10;
+const petesrHouseWidthInMeyer = 10;
+const volumeOfPetersHouseInMeter = petersHouselengthInMeter*petersHouseDepthInMeter*petesrHouseWidthInMeyer;
+ const petersHouseGardenSizeInM2 = 100;
+ const house1Cost = 2500000;
+ const housePrice = volumeOfPetersHouseInMeter * 2.5 * 1000 + petersHouseGardenSizeInM2 * 300;
+ if (house1Cost > housePrice) {
+    console.log(`Peter is paying too much. The calculated price is ${housePrice}, but he is paying ${house1Cost}.`);
+} else {
+    console.log(`Peter is paying too little. The calculated price is ${housePrice}, but he is paying ${house1Cost}.`);
+}
+ const juliasHouselengthInMeter = 8;
+const juliasHouseDepthInMeter = 11;
+const juliasHouseWidthInMeter = 5;
+const volumeOfjuliasHouseInMeter = juliasHouselengthInMeter*juliasHouseDepthInMeter*juliasHouseWidthInMeter;
+ const juliasHouseGardenSizeInM2 = 70;
+ const housePrice2 = volumeOfjuliasHouseInMeter * 2.5 * 1000 + juliasHouseGardenSizeInM2 * 300;
+ const house2Cost = 1000000;
+if (house2Cost>housePrice2){
+    console.log(`Julia is paying to high.The calculated price is ${housePrice2}, but she is paying ${house2Cost}`);
+}
+else{
+    console.log(`Julia is paying to low.The calculated price is ${housePrice2}, but she is paying ${house2Cost}`);
+}
