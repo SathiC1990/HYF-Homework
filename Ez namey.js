28dba889015543dbd905b302aa7630a7a3a584ea
sathic <sathichowdhury2017@gmail.com>
2024-06-06 13:08:09 +0200

    Implemented task 4

--- Ez namey.js
+++ Ez namey.js
@@ -18,7 +18,7 @@ const secondWords = ["Solutions",
 "Networks",
 "Dynamics",
 "Strategies"];
-const randomNumber1 = Math.floor(Math.random() * 10) +1;
-const randomNumber2 = Math.floor(Math.random() * 10) +1;
+const randomNumber1 = Math.floor(Math.random() * 10);
+const randomNumber2 = Math.floor(Math.random() * 10);
 const startupName = firstWords[randomNumber1] + " "  + secondWords[randomNumber2];
 console.log(`The startup: "${startupName}" contains ${startupName.length} characters`);
