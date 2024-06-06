8e94200d33ff1875f14e50589f74983d83120ab2
sathic <sathichowdhury2017@gmail.com>
2024-06-06 13:41:50 +0200

    implemeImplemented task 2

--- dog-age.js
+++ dog-age.js
@@ -1,5 +1,11 @@
-let dogYearOfBirth = 2018;
-let dogYearFuture =  2025;
-let dogYear = dogYearFuture - dogYearOfBirth;
- shouldShowResultInDogYears = "true";
-console. log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture);
+
+const dogYearOfBirth = 2017; 
+const dogYearFuture = 2027;  
+const shouldShowResultInDogYears = true; 
+const dogAgeInHumanYears = dogYearFuture - dogYearOfBirth;
+const dogAgeInDogYears = dogAgeInHumanYears * 15;
+if (shouldShowResultInDogYears) {
+    console.log(`Your dog will be ${dogAgeInDogYears} dog years old in ${dogYearFuture}.`);
+} else {
+    console.log(`Your dog will be ${dogAgeInHumanYears} human years old in ${dogYearFuture}.`);
+}
