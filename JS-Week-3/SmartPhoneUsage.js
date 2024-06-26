bb5eae4ddcdb0cca32fc19332736a7450fbd5473
sathic <sathichowdhury2017@gmail.com>
2024-06-26 14:50:18 +0200

    new change

--- JS-Week-3/SmartPhoneUsage.js
+++ JS-Week-3/SmartPhoneUsage.js
@@ -1,67 +1,48 @@
 const activities = [];
-
-
+const usageLimit = 100;
 function addActivity(date, activity, duration) {
-    const activityObject = {
-        date: new Date().toLocaleDateString(),
-        activity: activity,
-        duration: duration
-    };
-    activities.push(activityObject);
+  const activityObject = {
+    date: new Date().toLocaleDateString(),
+    activity: activity,
+    duration: duration,
+  };
+  activities.push(activityObject);
 }
 addActivity("23/7-18", "Youtube", 30);
 console.log(activities);
 
-let usageLimit = 100;
-function showStatus() {
-    if (activities.length === 0) {
-        return "Add some activities before calling showStatus";
-    }
-    
-    let totalActivities = activities.length;
-    let totalDuration = activities.reduce((sum, activity) => sum + activity.duration, 0);
-    
-    return `You have added ${totalActivities} activities. They amount to ${totalDuration} min. of usage`;
-}
-
-
-console.log(showStatus()); 
- 
-
 // Modified showStatus function with limit check
 function showStatus() {
-    if (activities.length === 0) {
-        return "Add some activities before calling showStatus";
-    }
-    
-    let totalActivities = activities.length;
-    let totalDuration = activities.reduce((sum, activity) => sum + activity.duration, 0);
-    
-    if (totalDuration > usageLimit) {
-        return "You have reached your limit, no more smartphoning for you!";
-    }
-    
-    return `You have added ${totalActivities} activities. They amount to ${totalDuration} min. of usage`;
+  if (activities.length === 0) {
+    return "Add some activities before calling showStatus";
+  }
+
+  const totalActivities = activities.length;
+  const totalDuration = activities.reduce(
+    (sum, activity) => sum + activity.duration,
+    0
+  );
+  if (totalDuration > usageLimit) {
+    return "You have reached your limit, no more smartphoning for you!";
+  }
+
+  return `You have added ${totalActivities} activities. They amount to ${totalDuration} min. of usage`;
 }
 
-/*function mostTimeSpentActivity() {
-    if (activities.length === 0) {
-        return "No activities to analyze";
-    }
-    
-    let maxActivity = activities.reduce((max, activity) => 
-        activity.duration > max.duration ? activity : max, 
-        activities[0]
-    );
-    
-    return `You have spent the most time on ${maxActivity.activity} with ${maxActivity.duration} min.`;
+function mostTimeSpentActivity() {
+  if (activities.length === 0) {
+    return "No activities to analyze";
+  }
+
+  let maxActivity = activities.reduce(
+    (max, activity) => (activity.duration > max.duration ? activity : max),
+    activities[0]
+  );
+
+  return `You have spent the most time on ${maxActivity.activity} with ${maxActivity.duration} min.`;
 }
 
-
-
-
-
-
+console.log(showStatus());
 
 // Example usage with more activities
 addActivity("23/7-18", "Running", 45);
