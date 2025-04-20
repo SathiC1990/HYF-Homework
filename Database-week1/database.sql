e5f9ab321facd57cada138aef57bf3eb58b61d00
sathic <sathichowdhury2017@gmail.com>
2025-04-20 23:28:40 +0200

    Changes

--- Database-week1/database.sql
+++ Database-week1/database.sql
@@ -1,17 +1,17 @@
 --Find out how many tasks are in the task table
-SELECT COUNT(*)from task;
+SELECT COUNT(*) AS total_task FROM task;
 --Find out how many tasks in the task table do not have a valid due date
 SELECT COUNT(*) from task where due_date is null;
 --Find all the tasks that are marked as done
 SELECT t.* 
 FROM task t    
-LEFT JOIN status s ON s.id = t.status_id
+INNER JOIN status s ON s.id = t.status_id
 WHERE s.name = 'Done';
 
 --Find all the tasks that are not marked as done
 SELECT t.* 
 FROM task t    
-LEFT JOIN status s ON s.id = t.status_id
+INNER JOIN status s ON s.id = t.status_id
 WHERE s.name != 'Done';
 --Get all the tasks, sorted with the most recently created first
 SELECT* from task ORDER BY created DESC;
@@ -19,7 +19,7 @@ SELECT* from task ORDER BY created DESC;
 SELECT * from task ORDER BY created DESC LIMIT 1;
 
 --Get the title and due date of all tasks where the title or description contains database
-select title, due_date from task where title LIKE  '%database%' OR description LIKE '%database%'; 
+select title, due_date FROM task WHERE title LIKE  '%database%' OR description LIKE '%database%'; 
  
 --Get the title and status (as text) of all tasks
 select t.title, s.name from task t                                     
